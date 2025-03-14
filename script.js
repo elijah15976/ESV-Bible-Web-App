759523382c63f4504ce31a14b3583cef6197126f
Elijah15976 <eren5836@baysidehighschool.org>
2025-03-13 21:00:26 -0400

    Parsed Bible passage. Handled getting passage from backend. Some style changes too

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 let output = document.querySelector("main");
 
 window.onload = () => {
+  let directory = "";
 
+  for(let book in bible){
+    directory += `<div class="menuBookName"><h3>${bookMap[book]}</h3>`;
+    directory += `<hr><div class="menuChapContainer">`;
+    for (let chap in bible[book]){
+      directory += `<div class="menuChap" onclick="loadPassage('${book}', '${chap}')">${chap}</div>`;
+    }
+    directory += "</div></div>";
+  }
+
+  document.getElementById("chapterSelections").innerHTML = directory;
+}
+
+function loadPassage(book, chapter){
+  output.innerHTML = bible[book][chapter];
 }
